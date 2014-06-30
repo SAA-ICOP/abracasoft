977c78c84a26996224bc275e0a82aaf226b924f8
Administrador <Administrador@Licitaciones8>
2014-06-30 12:59:45 -0300

    AbraBackUp actualizado con nueva clave de base de datos y el método para restaurar No funciona en todos los equipos, debe editarse la ruta de ubicacion del mysqldump y mysql Sirve de base para empezar a trabajar luego sobre ellos.
     30/06 12:59hs

--- src/gestores/AbraBackUp.java
+++ src/gestores/AbraBackUp.java
@@ -3,64 +3,143 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gestores;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+/*
+Este gestor funciona bien, habría que idear la forma de no necesitar crear el directorio backup.
+
+Lo métodos los dejé así nomás para darles libertad de recibir o no algún dato adicional para crear la base de
+datos (podrían decirle al usuario que agregue el nombre del archivo u otra cosa
+
+Creo que si mysqldump y mysql son agragados a las "variables del entorno" pueden utilizarse sin mencionar toda 
+la ruta de ubicación del archivo.
+
+*/
+package gestores;
 
 /**
  *
- * @author Ema
+ * @author Pablo
  */
-public class AbraBackUp {
 
-    private int BUFFER = 10485760;
+import java.awt.HeadlessException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
+import java.text.DateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+
+public class AbraBackUp {
+    private int BUFFER = 10485760;  
     //para guardar en memmoria
     private StringBuffer temp = null;
     //para guardar el archivo SQL
-    private FileWriter fichero = null;
+    private FileWriter  fichero = null;
     private PrintWriter pw = null;
+    
+    public boolean CrearBackup(){ //Crea un backup en "C:\backup\abracasoftDB+fecha.sql", esta previsto que sea solo uno por dia y si se ejecuta dos veces pisa al anterior
+        boolean ok=false;
+        Date hoy = new Date();
+        DateFormat formato = DateFormat.getDateInstance(DateFormat.LONG);
+        String fecha = formato.format(hoy);
+                
+    //    System.out.println(fecha);
+   
+        try{       
+        //sentencia para crear el BackUp, en mi computadora tuve que poner la ruta completa donde se encuentra
+        //instalado el archivo "mysqldump", el ejemplo de donde lo extraje solo decÃ­a "mysqldump".
+             Process run = Runtime.getRuntime().exec(
+            "C:\\Archivos de programa\\MySQL\\MySQL Server 5.0\\bin\\mysqldump --host=localhost --port=3306 --user=root --password=admin --complete-insert --extended-insert --skip-quote-names --skip-comments --skip-triggers abracasoftdb"); //--compact (le quite esto ultimo y anda mejor la restauracion, asi que listo el pollo, compact queda afuera
 
-    public boolean CrearBackup(String host, String port, String user, String password, String db, String directorio_backup) {
-        boolean ok = false;
-        try {
-            //sentencia para crear el BackUp, en mi computadora tuve que poner la ruta completa donde se encuentra
-            //instalado el archivo "mysqldump", el ejemplo de donde lo extraje solo decía "mysqldump".
-            Process run = Runtime.getRuntime().exec(
-                 "C:\\Archivos de programa\\MySQL\\MySQL Server 5.0\\bin\\mysqldump --host=" + host + " --port=" + port
-                    + " --user=" + user + " --password=" + password
-                    + " --compact --complete-insert --extended-insert --skip-quote-names"
-                    + " --skip-comments --skip-triggers " + db);
-            //se guarda en memoria el backup
+         //se guarda en memoria el backup
             InputStream in = run.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             temp = new StringBuffer();
             int count;
+/**/            temp.append("SET FOREIGN_KEY_CHECKS = 0;\n");
             char[] cbuf = new char[BUFFER];
-            while ((count = br.read(cbuf, 0, BUFFER)) != -1) {
+            while ((count = br.read(cbuf, 0, BUFFER)) != -1)
                 temp.append(cbuf, 0, count);
-            }
+/**/            temp.append("SET FOREIGN_KEY_CHECKS = 1;");
             br.close();
-            in.close();
-            // se crea y escribe el archivo SQL
-            fichero = new FileWriter(directorio_backup);
-            pw = new PrintWriter(fichero);
-            pw.println(temp.toString());
-            ok = true;
-        } catch (Exception ex) {
+            in.close();        
+        // se crea y escribe el archivo SQL
+            fichero = new FileWriter("c:\\backup\\" + "abracasoftDB " + fecha + ".sql");
+            pw = new PrintWriter(fichero);                                                    
+            pw.println(temp.toString());  
+            ok=true;
+        }
+        catch (Exception ex){
             ex.printStackTrace();
         } finally {
-            try {
-                if (null != fichero) {
+            try {           
+                if (null != fichero)
                     fichero.close();
-                }
-            } catch (Exception e2) {
+            } catch (IOException e2) {
                 e2.printStackTrace();
             }
+        }   
+        return ok; 
+        
+    //Para invocar este mÃ©todo habrÃ­a que hacerlo mediante un "new AbraBackUp().CrearBackup();"
+    }
+    
+    public boolean RestaurarBackup (String fecha){ // "S" es el nombre del archivo a restaurar;
+        boolean ok=false;
+        try {
+            /*NOTE: String s is the mysql file name including the .sql in its name*/
+            /*NOTE: Getting path to the Jar file being executed*/
+            /*NOTE: YourImplementingClass-> replace with the class executing the code*/
+            CodeSource codeSource = AbraBackUp.class.getProtectionDomain().getCodeSource();
+            File jarFile = new File(codeSource.getLocation().toURI().getPath());
+            String jarDir = jarFile.getParentFile().getPath();
+
+            /*NOTE: Creating Database Constraints*/
+             String dbName = "abracasoftdb";
+             String dbUser = "root";
+             String dbPass = "admin";
+             String nombreCorrecto = "abracasoftdb " + fecha +".sql";
+            /*NOTE: Creating Path Constraints for restoring*/
+          //  String restorePath = jarDir + "\\backup" + "\\" + s;
+             String restorePath="c:\\backup\\" + nombreCorrecto;
+
+            /*NOTE: Used to create a cmd command*/
+            /*NOTE: Do not create a single large string, this will cause buffer locking, use string array*/
+            String[] executeCmd = new String[]{"C:\\Archivos de programa\\MySQL\\MySQL Server 5.0\\bin\\mysql", dbName, "-u" + dbUser, "-p" + dbPass, "-e", " source " + restorePath};
+
+            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
+            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+
+            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
+            if (processComplete == 0) {
+                JOptionPane.showMessageDialog(null, "El backup de fecha: " + fecha + " correctamente restaurado");
+                ok=true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Error at restoring");
+            }
+            
+        } catch (URISyntaxException | IOException | InterruptedException | HeadlessException ex) {
+            JOptionPane.showMessageDialog(null, "Error at Restoredbfromsql" + ex.getMessage());
         }
         return ok;
     }
 }
+
+/*
+
+SET FOREIGN_KEY_CHECKS = 0; <- esto sirve para evitar problemas al actualizar la base de datos, va al principio
+
+
+SET FOREIGN_KEY_CHECKS = 1; <- esto va al final
+
+*/
+
+//drop schema abracasoftdb; <- para eliminar base de datos.
