c20587c37299473ad839a3e309ed8401e8808c22
German <German@N56VB>
2016-08-07 16:46:44 -0300

    Si

--- src/pantallas/MenuDeOpcionesEmpleado.java
+++ src/pantallas/MenuDeOpcionesEmpleado.java
@@ -222,11 +222,7 @@ public class MenuDeOpcionesEmpleado extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        GestorUsuario gestiondeusuario = new GestorUsuario();
-        if (señalPresu == 0) {
-            jTabbedPane1.addTab("Gestion Presupuesto", gestiondeusuario.getContentPane());
-           señalPresu = 1;
-        }
+        
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
